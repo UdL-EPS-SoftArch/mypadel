0f0e10403b7e7af0f2cf0d0322ccfa06a266b85d
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-22 16:13:55 +0100

    Created the match object

---
+++ src/app/match/Match.ts
@@ -0,0 +1,21 @@
+import {CourtType} from './CourtType';
+import {Player} from '../admin/player';
+
+export class Match{
+  id: number;
+  startDate: String;
+  duration: String;
+  courtType: CourtType;
+  cancelationDeadline: String;
+  isCancelled: boolean;
+  matchCreator: Player;
+  invitations: any = [];
+  /*
+  reservation: Reservation;
+  */
+
+
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
+}
