40235472bfa92d6a657693a13f4ba19c886523da
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-27 18:38:20 +0100

    added GET ALL method

--- src/app/public-match/PublicMatch.service.ts
+++ src/app/public-match/PublicMatch.service.ts
@@ -5,7 +5,6 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import {Observable} from "rxjs/Observable";
-import {Admin} from "../admin/admin";
 import { environment } from '../../environments/environment';
 import {PublicMatch} from "./PublicMatch";
 
@@ -13,6 +12,13 @@ import {PublicMatch} from "./PublicMatch";
 export class PublicMatchService {
   constructor(private http: Http,
               private authentication: AuthenticationBasicService) {}
+
+  // GET /publicMatches
+  getAllPublicMatches(): Observable<PublicMatch[]> {
+    return this.http.get(`${environment.API}/publicMatches`)
+      .map((res: Response) => res.json()._embedded.publicMatches.map(json => new PublicMatch(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
   // GET /publicMatches /id
   getPublicMatch(id: string): Observable<PublicMatch> {
