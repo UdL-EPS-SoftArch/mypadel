7caa3fbc5b9b5403ebc987719cb5b5bbe538be44
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-27 19:45:58 +0100

    created component

---
+++ src/app/public-match/public-match-list/public-match.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {PublicMatch} from "../PublicMatch";
+import {PublicMatchService} from "../PublicMatch.service";
+
+@Component({
+  selector: 'app-publicMatch-list',
+  templateUrl: './public-match-list.component.html'
+})
+export class PublicMatchListComponent implements OnInit {
+  public publicMatches: PublicMatch[] = [];
+  public totalPublicMatches: number;
+  public errorMessage = '';
+
+  constructor(private publicMatchService: PublicMatchService) {}
+
+  ngOnInit() {
+    this.publicMatchService.getAllPublicMatches()
+      .subscribe(
+        (publicMatches: PublicMatch[]) => {
+          this.publicMatches = publicMatches;
+          this.totalPublicMatches = publicMatches.length; },
+        error => this.errorMessage = <any>error.message);
+  }
+
+  onSearch(publicMatches) {
+    this.publicMatches = publicMatches;
+  }
+}
