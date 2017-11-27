6b68806743f1ff8390e3e388345034bc59fa6b06
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-27 18:31:27 +0100

    Added get method

--- src/app/public-match/PublicMatch.service.ts
+++ src/app/public-match/PublicMatch.service.ts
@@ -1,6 +1,29 @@
 import {Injectable} from "@angular/core";
+import {AuthenticationBasicService} from "../login-basic/authentication-basic.service";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import {Observable} from "rxjs/Observable";
+import {Admin} from "../admin/admin";
+import { environment } from '../../environments/environment';
+import {PublicMatch} from "./PublicMatch";
 
 @Injectable()
 export class PublicMatchService {
+  constructor(private http: Http,
+              private authentication: AuthenticationBasicService) {}
+
+  // GET /publicMatches /id
+  getPublicMatch(id: string): Observable<PublicMatch> {
+    return this.http.get(`${environment.API}/publicMatches/${id}`)
+      .map((res: Response) => new PublicMatch(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+
+
+
+
 
 }
