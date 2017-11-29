1938c7219254dba4e5b55086a01fdff5d38acc68
Roger Arques <rarquesvall@gmail.com>
2017-11-29 12:26:05 +0100

    Generated component to display all courts (court-list)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AdminDeleteComponent } from './admin/admin-delete/admin-delete.componen
 import { PublicMatchService } from './public-match/PublicMatch.service';
 import { PublicMatchSearchComponent } from './public-match/public-match-search/public-match-search.component';
 import { PublicMatchListComponent } from './public-match/public-match-list/public-match-list.component';
+import { CourtListComponent } from './court/court-list/court-list.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { PublicMatchListComponent } from './public-match/public-match-list/publi
     AdminSearchComponent,
     AdminDeleteComponent,
     PublicMatchSearchComponent,
-    PublicMatchListComponent
+    PublicMatchListComponent,
+    CourtListComponent
   ],
   imports: [
     BrowserModule,
