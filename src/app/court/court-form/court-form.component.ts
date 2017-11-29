be2f7b511e1d900d84c88167856bbd59ac63fd53
Roger Arques <rarquesvall@gmail.com>
2017-11-29 16:50:38 +0100

    Court form -> error 'No provider for CourtService'

--- src/app/court/court-form/court-form.component.ts
+++ src/app/court/court-form/court-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Court} from '../Court';
+import {Router} from '@angular/router';
+import {CourtService} from '../court.service';
 
 @Component({
   selector: 'app-court-form',
   templateUrl: './court-form.component.html',
-  styleUrls: ['./court-form.component.css']
+  styleUrls: ['./court-form.component.css'],
 })
 export class CourtFormComponent implements OnInit {
 
-  constructor() { }
+  public court: Court;
+  public courtForm: FormGroup;
+  public errorMessage: string;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private courtService: CourtService) {
+    this.courtForm = fb.group({
+      'isIndoor': ['Is court indoor?', Validators.required],
+      'available': ['Availability', Validators.required]
+    });
+  }
 
   ngOnInit() {
+    this.court = new Court();
   }
+
+  onSubmit(): void {
+    this.courtService.addCourt(this.court)
+      .subscribe(
+        court => this.router.navigate(['/courts/' + court.id]),
+        error => {
+          this.errorMessage = error.errors ? <any>error.errors[0].message : <any>error.message;
+        });
+  }
+
 
 }
