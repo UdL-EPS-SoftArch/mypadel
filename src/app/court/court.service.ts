26458e372357ec59f71b77ae0f535d3ecabc4ee1
Roger Arques <rarquesvall@gmail.com>
2017-11-29 12:12:55 +0100

    Created court service with GET all method

---
+++ src/app/court/court.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
+import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment';
+import {Court} from './Court';
+
+@Injectable()
+export class CourtService {
+
+  constructor(private http: Http,
+              private authentication: AuthenticationBasicService) {
+  }
+
+  // TODO: GET all courts
+  getAllCourts(): Observable<Court[]> {
+    return this.http.get(`${environment.API}/courts`)
+      .map((res: Response) => res.json()._embedded.courts.map(json => new Court(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // TODO: GET court by id
+  // TODO: POST a new court
+}
