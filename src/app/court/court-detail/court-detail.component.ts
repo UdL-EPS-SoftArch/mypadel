10c181cdf9ccc23c1d7d9a0866c17beb40105f85
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-04 00:26:15 +0100

    Added court-detail component

---
+++ src/app/court/court-detail/court-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-court-detail',
+  templateUrl: './court-detail.component.html',
+  styleUrls: ['./court-detail.component.css']
+})
+export class CourtDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
