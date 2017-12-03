afb282cfca31b9e9deb75f9a70f34cec6231b344
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-04 00:27:02 +0100

    Added court-search component

---
+++ src/app/court/court-search/court-search.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Court} from '../Court';
+import {CourtService} from '../court.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-court-search',
+  templateUrl: './court-search.component.html',
+  styleUrls: ['./court-search.component.css']
+})
+export class CourtSearchComponent implements OnInit {
+  @Input()
+  courts: Court[];
+  @Output()
+  onSearchited: EventEmitter<any> = new EventEmitter();
+  public errorMessage: string;
+
+  constructor(private courtService: CourtService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+  }
+
+  performSearch(searchTerm: string): void {
+    this.courtService.getCourt(searchTerm).subscribe(
+      courts => {
+        this.onSearchited.emit(courts);
+      },
+      error => this.errorMessage = <any>error.message
+    );
+  }
+}
