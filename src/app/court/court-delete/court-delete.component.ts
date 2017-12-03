4e930662d570a0c33099ca1ec69709fbda001da4
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-04 00:24:53 +0100

    Added court-delete component

---
+++ src/app/court/court-delete/court-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-court-delete',
+  templateUrl: './court-delete.component.html',
+  styleUrls: ['./court-delete.component.css']
+})
+export class CourtDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
