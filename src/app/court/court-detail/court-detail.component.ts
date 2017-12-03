7f830e2b87bf2ddf06cc51232df010b5fccffdec
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-04 00:55:32 +0100

    Detail view, minor changes

--- src/app/court/court-detail/court-detail.component.ts
+++ src/app/court/court-detail/court-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Court} from '../Court';
+import {ActivatedRoute} from '@angular/router';
+import {CourtService} from '../court.service';
 
 @Component({
   selector: 'app-court-detail',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./court-detail.component.css']
 })
 export class CourtDetailComponent implements OnInit {
+  public court: Court;
+  public errorMessage: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private courtService: CourtService) {
+  }
 
   ngOnInit() {
+    this.route.params
+      .map(params => params['id'])
+      .subscribe((id) => {
+          this.courtService.getCourt(`${id}`).subscribe(
+            court => this.court = court,
+            error => this.errorMessage = <any>error.message);
+        }
+      );
   }
 
 }
