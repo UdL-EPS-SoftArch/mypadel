1be024466084259662fa762f150c9393273c272b
Roger Arques <rarquesvall@gmail.com>
2017-12-03 21:23:13 +0100

    Removed unused import

--- src/app/court/court-form/court-form.component.ts
+++ src/app/court/court-form/court-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup} from '@angular/forms';
 import {Court} from '../Court';
 import {Router} from '@angular/router';
 import {CourtService} from '../court.service';
