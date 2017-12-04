4d14530e63ef4498112e9488b92ebc1cbe8bf90c
Marc Ribalta Gene <mariva1995@gmail.com>
2017-12-04 21:25:22 +0100

    corrected the ng lint type errors

--- e2e/features/steps/public-match/create-public-match.steps.ts
+++ e2e/features/steps/public-match/create-public-match.steps.ts
@@ -10,11 +10,11 @@ class CreatePublicMatchSteps {
   private datasetForm = new PublicMatchFormPage();
 
   @when(/^I create a public match with date "([^"]*)", court type "([^"]*)" and level "([^"]*)"$/)
-  public iCreateAPublicMatch(date: string, courtType: string, level: string, callback): void{
+  public iCreateAPublicMatch (date: string, courtType: string, level: string, callback): void{
     element(by.linkText('Public Matches')).click();
     element(by.linkText('Create New Public Match')).click();
-    // this.datasetForm.setStartDate(date);
-    this.datasetForm.setDuration("PT60M");
+    this.datasetForm.setStartDate(date);
+    this.datasetForm.setDuration('PT60M');
     this.datasetForm.setCourtType(courtType);
     this.datasetForm.setLevel(level);
     this.datasetForm.submitForm();
