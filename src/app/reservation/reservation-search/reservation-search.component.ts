cadf50f5c74d5227433100a40af50239ec4ae281
cclauwers <cclauwers@gmail.com>
2017-12-05 15:31:52 +0100

    button text of reservation list modified reservation-search.component.html and reservation-search.component.ts created

---
+++ src/app/reservation/reservation-search/reservation-search.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Reservation } from "../reservation";
+import { ReservationService } from "../reservation.service";
+
+@Component({
+  selector: 'app-reservation-search',
+  templateUrl: './reservation-search.component.html',
+  styleUrls: ['./reservation-search.component.css']
+})
+export class ReservationSearchComponent{
+
+  @Input()
+  reservations: Reservation[];
+  @Output()
+  onSearchited: EventEmitter<any> = new EventEmitter();
+
+  public errorMessage: string;
+  constructor(private reservationService: ReservationService,
+              private route: ActivatedRoute) {
+  }
+
+  performSearch(searchTerm: string): void {
+    this.reservationService.getReservationsByStartDate(searchTerm).subscribe(
+      reservations => { this.onSearchited.emit(reservations); },
+      error => this.errorMessage = <any>error.message);
+  }
+}
