2e5ee1f680159c0271813cf7f14d1ae8b6b1ffc7
joanpaucg <joanpau.castells@hotmail.com>
2017-12-09 16:58:40 +0100

    Create-player e2e Done!

--- e2e/features/8.create-player.feature
+++ e2e/features/8.create-player.feature
@@ -4,8 +4,9 @@ Feature: Create Administrator
   I want to register them
 
   Scenario: Create additional administrator
-    Given I sign in as "admin@mypadel.cat" with password "password"
+    Given I logout
+    And I sign in as "admin@mypadel.cat" with password "password"
     And I see 1 players
-    When I create a Player with username "player2" and e-mail "player2@mypadel.cat"
+    When I create a player with username "player2" and e-mail "player2@mypadel.cat"
     Then I click menu option "Players"
     And I see 2 players
---
+++ e2e/features/steps/player/create-player.steps.ts
@@ -0,0 +1,21 @@
+import { binding, given } from 'cucumber-tsflow';
+import { browser, element, by } from 'protractor';
+import {PlayerFormPage} from "../../../pages/player/player-form.page";
+
+
+@binding()
+class CreatePlayerSteps {
+  private datasetForm = new PlayerFormPage();
+
+  @given(/^I create a player with username "([^"]*)" and e-mail "([^"]*)"$/)
+  public createDatasetWithTitleAndDescription (username: string, email: string, callback): void {
+    element(by.linkText('Register New Player')).click();
+    this.datasetForm.setUsername(username);
+    this.datasetForm.setEMail(email);
+    this.datasetForm.submitForm();
+    browser.waitForAngular();
+    callback();
+  }
+}
+
+export = CreatePlayerSteps;
