8b59763d0a81b65801be338d234e997d9fec408e
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-11 22:48:17 +0100

    generated private-match-list component

---
+++ src/app/private-match/private-match-list/private-match-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-match-list',
+  templateUrl: './private-match-list.component.html',
+  styleUrls: ['./private-match-list.component.css']
+})
+export class PrivateMatchListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
