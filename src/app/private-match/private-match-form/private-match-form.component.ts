f28234198325a9e49af17b3c60aac9f96b9d7649
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-11 22:46:25 +0100

    generated private-match-form component

---
+++ src/app/private-match/private-match-form/private-match-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-match-form',
+  templateUrl: './private-match-form.component.html',
+  styleUrls: ['./private-match-form.component.css']
+})
+export class PrivateMatchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
