e56f826394b971d6958f0810e2ba35f3f9a910f2
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-11 22:48:44 +0100

    generated private-match-search component

---
+++ src/app/private-match/private-match-search/private-match-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-match-search',
+  templateUrl: './private-match-search.component.html',
+  styleUrls: ['./private-match-search.component.css']
+})
+export class PrivateMatchSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
