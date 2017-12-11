04cfe95676f6c36a18ba6066b211071d2de7ab51
ratchetmdt <mdt3@alumnes.udl.cat>
2017-12-11 22:47:44 +0100

    generated private-match-edit component

---
+++ src/app/private-match/private-match-edit/private-match-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-match-edit',
+  templateUrl: './private-match-edit.component.html',
+  styleUrls: ['./private-match-edit.component.css']
+})
+export class PrivateMatchEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
