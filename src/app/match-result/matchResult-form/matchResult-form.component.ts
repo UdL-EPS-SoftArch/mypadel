ad4aca23dbd6a7763d5eb8ce1e90a176d3f3985a
poltorres0999 <eichgammer@gmail.com>
2017-12-12 12:38:43 +0100

    Created matchResult form component

---
+++ src/app/match-result/matchResult-form/matchResult-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {MatchResult} from '../MatchResult';
+import {MatchResultService} from '../MatchResult.service';
+
+@Component({
+  selector: 'app-matchResult-form',
+  templateUrl: './matchResult-form.component.html'
+})
+export class PublicMatchFormComponent implements OnInit {
+  public matchResult: MatchResult;
+  public matchResultForm: FormGroup;
+  public errorMessage: string;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private matchResultService: MatchResultService) {
+    this.matchResultForm = fb.group({
+      'winningPair': ['The Winning pair', Validators.required],
+      'losingPair': ['The Loosing pair', Validators.required],
+      'isDraw': ['Draw', Validators.required],
+      'match': ['Match', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.matchResult = new MatchResult();
+  }
+
+  onSubmit(): void {
+    this.matchResultService.addMatchResult(this.matchResult)
+      .subscribe(
+        matchResult => this.router.navigate([matchResult.uri]),
+        error => {
+          this.errorMessage = error.errors ? <any>error.errors[0].message : <any>error.message;
+        });
+  }
+}
