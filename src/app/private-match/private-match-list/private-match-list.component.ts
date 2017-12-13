2e2fb8961ccdafcc0c075184b04067943082aaff
naluem <mdt3@alumnes.udl.cat>
2017-12-13 21:10:03 +0100

    created private-match-list.component.ts

--- src/app/private-match/private-match-list/private-match-list.component.ts
+++ src/app/private-match/private-match-list/private-match-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {PrivateMatch} from '../PrivateMatch';
+import {PrivateMatchService} from '../PrivateMatch.service';
 
 @Component({
   selector: 'app-private-match-list',
-  templateUrl: './private-match-list.component.html',
-  styleUrls: ['./private-match-list.component.css']
+  templateUrl: './private-match-list.component.html'
 })
 export class PrivateMatchListComponent implements OnInit {
+  public PrivateMatches: PrivateMatch[] = [];
+  public totalPrivateMatches = 0;
+  public errorMessage = '';
 
-  constructor() { }
+  constructor(private PrivateMatchService: PrivateMatchService) {}
 
   ngOnInit() {
+    this.PrivateMatchService.getAllPrivateMatches()
+      .subscribe(
+        (PrivateMatches: PrivateMatch[]) => {
+          this.PrivateMatches = PrivateMatches;
+          this.totalPrivateMatches = PrivateMatches.length; },
+        error => this.errorMessage = <any>error.message);
   }
 
+  onSearch(PrivateMatches) {
+    this.PrivateMatches = PrivateMatches;
+  }
 }
