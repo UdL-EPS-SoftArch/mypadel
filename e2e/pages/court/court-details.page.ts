1290679b7665df60b58f56ef36de1e9e54e81ed4
Roger Arques Vall <rarquesvall@gmail.com>
2017-12-14 00:43:04 +0100

    class to represent the detail view of a court

---
+++ e2e/pages/court/court-details.page.ts
@@ -0,0 +1,30 @@
+import { element, by, ElementFinder } from 'protractor';
+import { promise } from 'selenium-webdriver';
+
+export class CourtDetailsPage {
+
+  private detailsBody: ElementFinder;
+  private available: string;
+  private indoor: string;
+
+  constructor() {
+    this.detailsBody = element.all(by.css('div.panel-body')).first();
+    this.parseValues();
+  }
+
+  private parseValues() {
+    let text: string;
+    this.detailsBody.getText().then((str: string) => text = str);
+    const parts = text.split(' | ');
+    this.indoor = parts[0];
+    this.available = parts[1];
+  }
+
+  getAvailable(): string {
+    return this.available;
+  }
+
+  getIndoor(): string {
+    return this.indoor;
+  }
+}
