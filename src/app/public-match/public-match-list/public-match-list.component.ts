28194889c28d4b05749a93a1bbb74c2b452274c8
Marc Ribalta Gene <mariva1995@gmail.com>
2017-12-14 12:25:37 +0100

    Formated the duration of a public match so it can be shown correctly

--- src/app/public-match/public-match-list/public-match-list.component.ts
+++ src/app/public-match/public-match-list/public-match-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {PublicMatch} from '../PublicMatch';
 import {PublicMatchService} from '../PublicMatch.service';
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-public-match-list',
@@ -17,9 +18,17 @@ export class PublicMatchListComponent implements OnInit {
     this.publicMatchService.getAllPublicMatches()
       .subscribe(
         (publicMatches: PublicMatch[]) => {
-          this.publicMatches = publicMatches;
+          this.publicMatches = this.formattedPublicMatches(publicMatches);
+          console.log(this.publicMatches);
           this.totalPublicMatches = publicMatches.length; },
         error => this.errorMessage = <any>error.message);
+  }
+
+  private formattedPublicMatches(publicMatches: PublicMatch[]): PublicMatch[] {
+    publicMatches.forEach((match)=>{
+      match.duration = match.duration.split("PT")[1];
+    });
+    return publicMatches;
   }
 
   onSearch(publicMatches) {
