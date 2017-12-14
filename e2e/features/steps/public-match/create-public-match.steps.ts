4795dc999101c4b6e267916b39abeeed2e13baa6
Marc Ribalta Gene <mariva1995@gmail.com>
2017-12-14 21:24:22 +0100

    Changed the way we enter in the form page

--- e2e/features/steps/public-match/create-public-match.steps.ts
+++ e2e/features/steps/public-match/create-public-match.steps.ts
@@ -1,22 +1,23 @@
 import {binding, when} from 'cucumber-tsflow';
 import {browser, by, element} from 'protractor';
 import {PublicMatchFormPage} from '../../../pages/public-match/public-match-form.page';
+import {MainContentPage} from "../../../pages/main-content.page";
 
 const chai = require('chai').use(require('chai-as-promised'));
 const expect = chai.expect;
 
 @binding()
 class CreatePublicMatchSteps {
-  private datasetForm = new PublicMatchFormPage();
+  private datasetForm;
 
   @when(/^I create a public match with date "([^"]*)", court type "([^"]*)" and level "([^"]*)"$/)
   public iCreateAPublicMatch (date: string, courtType: string, level: string, callback): void {
-    element(by.linkText('Public Matches')).click();
-    element(by.linkText('Create New Public Match')).click();
+    new MainContentPage().clickLinkWithText('Create New Public Match');
+    this.datasetForm = new PublicMatchFormPage();
     this.datasetForm.setStartDate(date);
-    this.datasetForm.setDuration('PT60M');
-    this.datasetForm.setCourtType(courtType);
-    this.datasetForm.setLevel(level);
+    // this.datasetForm.setDuration('PT60M');
+    // this.datasetForm.setCourtType(courtType);
+    // this.datasetForm.setLevel(level);
     this.datasetForm.submitForm();
     browser.waitForAngular();
     callback();
