d24a8ba87b60bac5e68b2d129f71f048b0125f48
Marc Ribalta Gene <mariva1995@gmail.com>
2017-12-14 16:06:26 +0100

    "->'

--- src/app/match/Match.service.ts
+++ src/app/match/Match.service.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {AuthenticationBasicService} from '../login-basic/authentication-basic.service';
-import {Observable} from "rxjs/Observable";
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
-import {environment} from "../../environments/environment";
-import {Match} from "./Match";
+import {environment} from '../../environments/environment';
+import {Match} from './Match';
 
 @Injectable()
 export class MatchService {
