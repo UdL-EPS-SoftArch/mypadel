c492a5d359bd69a94c05bf22fb613aede2c2a293
antonio <antonioexposito6@gmail.com>
2017-12-15 00:44:52 +0800

    Fixed syntax errors v3.

--- src/app/join-match/JoinMatch.ts
+++ src/app/join-match/JoinMatch.ts
@@ -2,7 +2,7 @@ import { Player } from '../player/player';
 import { PublicMatch } from '../public-match/PublicMatch';
 import { CustomMatch } from '../custom-match/custom-match';
 
-export class JoinMatch{
+export class JoinMatch {
   uri: string;
   id: number;
   date_match: Date;
