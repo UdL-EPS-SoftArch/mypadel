050553a7f2b5b46f684456ca53851ee5757a8933
antonio <antonioexposito6@gmail.com>
2017-12-20 03:35:51 +0800

    Fixed errors of ng lint v2

--- src/app/join-match/JoinMatch.ts
+++ src/app/join-match/JoinMatch.ts
@@ -1,7 +1,7 @@
 import { Player } from '../player/player';
 import { PublicMatch } from '../public-match/PublicMatch';
 import { CustomMatch } from '../custom-match/custom-match';
-import {Match} from "../match/Match";
+import {Match} from '../match/Match';
 
 export class JoinMatch {
   uri: string;
