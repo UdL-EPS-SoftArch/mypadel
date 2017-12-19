8fed64ef0a72973b4d2d0ce3916e0b89cf1de0df
cclauwers <cclauwers@gmail.com>
2017-12-19 16:34:04 +0100

    reservation-list test

---
+++ e2e/features/steps/reservation/list-reservation.steps.ts
@@ -0,0 +1,18 @@
+import { binding, then } from 'cucumber-tsflow';
+import {ReservationListPage} from '../../../pages/reservation/reservation-list.page';
+
+const chai = require('chai').use(require('chai-as-promised'));
+const expect = chai.expect;
+
+@binding()
+class ListReservationSteps {
+  private reservationList = new ReservationListPage();
+
+  @then(/^I see (\d+) players/)
+  public iSeePlayers(count: string, callback): void {
+    expect(this.reservationList.getReservationCount())
+      .to.eventually.equal(parseInt(count, 10)).and.notify(callback);
+  }
+}
+
+export = ListReservationSteps;
