fd0306ca9e2d005af7fb3c068f9c463a3293c232
Roger Arques Vall <rarquesvall@gmail.com>
2017-12-20 23:27:55 +0100

    Init builder for different types of matches

---
+++ src/app/match/MatchBuilder.ts
@@ -0,0 +1,15 @@
+import {Level} from '../public-match/Level';
+import {CourtType} from './CourtType';
+
+export class MatchBuilder {
+
+  startDate: Date;
+  duration: string;
+  courtType: CourtType;
+  level: Level;
+
+  constructor() {
+
+  }
+
+}
