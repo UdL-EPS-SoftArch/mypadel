4cd32891b9f22cd5806c8efb3cc62702713384a4
Roger Arques Vall <rarquesvall@gmail.com>
2017-12-20 23:39:21 +0100

    Builder done

--- src/app/match/MatchBuilder.ts
+++ src/app/match/MatchBuilder.ts
@@ -1,5 +1,8 @@
 import {Level} from '../public-match/Level';
 import {CourtType} from './CourtType';
+import {PublicMatch} from '../public-match/PublicMatch';
+import {PrivateMatch} from '../private-match/PrivateMatch';
+import {CustomMatch} from '../custom-match/custom-match';
 
 export class MatchBuilder {
 
@@ -8,8 +11,8 @@ export class MatchBuilder {
   courtType: CourtType;
   level: Level;
 
-  constructor() {
-
+  static match(): MatchBuilder {
+    return new MatchBuilder();
   }
 
   withStartDate(startDate: Date): MatchBuilder {
@@ -30,6 +33,31 @@ export class MatchBuilder {
   withLevel(level: Level): MatchBuilder {
     this.level = level;
     return this;
+  }
+
+  asPublicMatch(): PublicMatch {
+    const publicMatch = new PublicMatch();
+    publicMatch.startDate = this.startDate;
+    publicMatch.duration = this.duration;
+    publicMatch.courtType = this.courtType;
+    publicMatch.level = this.level;
+    return publicMatch;
+  }
+
+  asPriateMatch(): PrivateMatch {
+    const privateMatch = new PrivateMatch();
+    privateMatch.startDate = this.startDate;
+    privateMatch.duration = this.duration;
+    privateMatch.courtType = this.courtType;
+    return privateMatch;
+  }
+
+  asCustomMatch(): CustomMatch {
+    const customMatch = new CustomMatch();
+    customMatch.startDate = this.startDate;
+    customMatch.duration = this.duration;
+    customMatch.courtType = this.courtType;
+    return customMatch;
   }
 
 }
