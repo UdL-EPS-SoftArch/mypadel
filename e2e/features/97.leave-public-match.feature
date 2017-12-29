4ed678528ff457e1d1e309e435206c5a75151df0
Albert <amr1990lol@gmail.com>
2017-12-29 11:36:43 +0100

    Join match e2e list, join to a public match and leave a public match from the public matches section tests done

--- e2e/features/97.leave-public-match.feature
+++ e2e/features/97.leave-public-match.feature
@@ -1,13 +1,13 @@
-Feature: Join a public match
-  In order to join a match
+Feature: Leave a public match
+  In order to cancel my attendance to a match
   As a player
-  I want to join it
+  I want to leave a match
 
-  Scenario: Join a public match
+  Scenario: Leave a public match
     Given I'm on the home page and logged out
     And I sign in as "player2@mypadel.cat" with password "password"
     When I click menu option "Public Matches"
     And I click to the match "1"
-    And I join to the match
+    When I leave the match
     Then I click menu option "My Games"
-    And I see 1 games
+    And I see 0 games
--- e2e/features/steps/public-match/details-public-match.steps.ts
+++ e2e/features/steps/public-match/details-public-match.steps.ts
@@ -1,12 +1,32 @@
-import {CourtDetailsPage} from '../../../pages/court/court-details.page';
-import {binding} from 'cucumber-tsflow';
+import {binding, when} from 'cucumber-tsflow';
 import {MainContentPage} from '../../../pages/main-content.page';
+import {browser} from 'protractor';
 
 const chai = require('chai').use(require('chai-as-promised'));
 const expect = chai.expect;
 
 @binding()
-class DetailCourtSteps {
+export class DetailPublicMatches {
   private mainContent = new MainContentPage();
-  private courtDetails = new CourtDetailsPage();
+
+  @when(/^I click to the match "([^"]*)"$/)
+  public WhenIClickMatchWithId (name: string, callback): void {
+    this.mainContent.clickLinkWithText(name);
+    browser.waitForAngular();
+    callback();
+  }
+
+  @when(/^I join to the match$/)
+  public WhenIClickJoin (callback): void {
+    this.mainContent.clickButtonWithText('Join');
+    browser.waitForAngular();
+    callback();
+  }
+
+  @when(/^I leave the match$/)
+  public iLeaveTheMatch(callback): void {
+    this.mainContent.clickButtonWithText('Leave the match');
+    browser.waitForAngular();
+    callback();
+  }
 }
