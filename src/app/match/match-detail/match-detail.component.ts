d2b72ac94a2f129648d4c91d38ac96f8293c6d92
joanpaucg <joanpau.castells@hotmail.com>
2018-01-13 01:07:17 +0100

    Match Matches with customMatch

--- src/app/match/match-detail/match-detail.component.ts
+++ src/app/match/match-detail/match-detail.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Match} from '../Match';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {MatchService} from '../Match.service';
+import {MatchJoinRequestService} from '../../match-join-request/match-join-request.service';
+import {MatchJoinRequest} from "../../match-join-request/MatchJoinRequest";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-match-detail',
@@ -9,10 +12,14 @@ import {MatchService} from '../Match.service';
 })
 export class MatchDetailComponent implements OnInit {
   public match: Match;
+  public matchJoinRequest:MatchJoinRequest;
   public errorMessage: string;
 
   constructor(private route: ActivatedRoute,
-              private matchService: MatchService) {
+              private matchService: MatchService,
+              private matchJoinRequestService:MatchJoinRequestService,
+              private router: Router,
+  ) {
   }
 
   ngOnInit() {
@@ -38,6 +45,30 @@ export class MatchDetailComponent implements OnInit {
       matchInvitations => this.match.invitations = matchInvitations,
       error => this.errorMessage = <any>error.message
     );
+  }
+  isLoggedIn(): boolean {
+    return this.matchService.isLoggedIn();
+  }
+  createMatchJoinRequest(): void {
+    this.matchJoinRequest = new MatchJoinRequest();
+    this.matchJoinRequest.message="Hi I want to join in your match !";
+    this.matchJoinRequest.customMatch=`${environment.API}/customMatches/${this.match.id}`
+    this.matchJoinRequest.eventDate=this.match.startDate;
+
+    this.matchJoinRequestService.addMatchJoinRequest(this.matchJoinRequest)
+      .subscribe(
+        matchJoinRequest => this.router.navigate([matchJoinRequest.uri]),
+
+        error => {
+          this.errorMessage = error.errors ? <any>error.errors[0].message : <any>error.message;
+        });
+
+
+
+  }
+  customMatchURI():string{
+    return `/customMatches/${this.match.id}`
+
   }
 
 }
