ce63e1a846c1fa81a269888b56640ca3fb187913
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-01-14 17:16:24 +0100

    services redone

--- src/app/match-result/MatchResult.service.ts
+++ src/app/match-result/MatchResult.service.ts
@@ -8,63 +8,72 @@ import { MatchResult } from './MatchResult';
 @Injectable()
 export class MatchResultService {
 
-    private readonly http: Http;
-    private readonly authenticationBasicService: AuthenticationBasicService;
+  private readonly http: Http;
+  private readonly authenticationBasicService: AuthenticationBasicService;
 
-    public constructor(http: Http, authenticationBasicService: AuthenticationBasicService) {
-        this.http = http;
-        this.authenticationBasicService = authenticationBasicService;
-    }
+  public constructor(http: Http, authenticationBasicService: AuthenticationBasicService) {
+    this.http = http;
+    this.authenticationBasicService = authenticationBasicService;
+  }
 
-    // Request Method: GET /matchResults -> Get all the MatchResults
-    public getAllMatchReults(): Observable<MatchResult[]> {
-        return this.http.get(`${environment.API}/matchResults`)
-            .map((res: Response): MatchResult => res.json())
-            .catch((error: any) => Observable.throw(error.json()));
-    }
+  // Request Method: GET /matchResults -> Get all the MatchResults
+  public getAllMatchReults(): Observable<MatchResult[]> {
+    return this.http.get(`${environment.API}/matchResults`)
+      .map((res: Response): MatchResult => res.json() as MatchResult)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
-    // Request Method: GET /matchResults/id -> Get the MatchResult with the given id
-    public getMatchResult(id: number): Observable<MatchResult> {
-        return this.http.get(`${environment.API}/matchResults/${id}`)
-            .map((res: Response): MatchResult => res.json())
-            .catch((error: any) => Observable.throw(error.json()));
-    }
+  // Request Method: GET /matchResults/id -> Get the MatchResult with the given id
+  public getMatchResult(id: number): Observable<MatchResult> {
+    return this.http.get(`${environment.API}/matchResults/${id}`)
+      .map((res: Response): MatchResult => res.json() as MatchResult)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
-    // Request Method: POST /matchResults/ -> add's a new MatchResult
-    public addMatchResult(matchResult: MatchResult): Observable<MatchResult> {
-        const body = JSON.stringify(matchResult);
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        headers.append('Authorization', this.authenticationBasicService.getCurrentUser().authorization);
-        const options = new RequestOptions({ headers: headers });
+  // Request Method: POST /matchResults/ -> add's a new MatchResult
+  public addMatchResult(matchResult: MatchResult): Observable<MatchResult> {
+    const body = JSON.stringify(matchResult);
+    const headers = new Headers([
+      { 'Content-Type': 'application/json' },
+      {'Authorization': this.authenticationBasicService.getCurrentUser().authorization}]
+    );
+    const options = new RequestOptions({ headers: headers });
 
-        return this.http.post(`${environment.API}/matchResults`, body, options)
-            .map((res: Response): MatchResult => res.json())
-            .catch((error: any) => Observable.throw(error.json()));
-    }
+    return this.http.post(`${environment.API}/matchResults`, body, options)
+      .map((res: Response): MatchResult => res.json() as MatchResult)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
-    // Request Method: PUT /matchResults/id -> update's the given MatchResult
-    public updateMatchResult(matchResult: MatchResult): Observable<MatchResult> {
+  // Request Method: PUT /matchResults/id -> update's the given MatchResult
+  public updateMatchResult(matchResult: MatchResult): Observable<MatchResult> {
+    const body = JSON.stringify(matchResult);
+    const headers = new Headers([
+      { 'Content-Type': 'application/json' },
+      {'Authorization': this.authenticationBasicService.getCurrentUser().authorization}]
+    );
+    const options = new RequestOptions({ headers: headers });
 
-        const body = JSON.stringify(matchResult);
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        headers.append('Authorization', this.authenticationBasicService.getCurrentUser().authorization);
-        const options = new RequestOptions({ headers: headers });
+    return this.http.put(`${environment.API}/${matchResult.id}`, body, options)
+      .map((res: Response): MatchResult => res.json() as MatchResult)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
-        return this.http.put(`${environment.API}/${matchResult.id}`, body, options)
-            .map((res: Response): MatchResult => res.json())
-            .catch((error: any) => Observable.throw(error.json()));
-    }
+  /**
+   * DELETE /admin/{id}
+   * @param {MatchResult} matchResult
+   * @returns {Observable<Response>}
+   */
+  public deleteMatchResult(matchResult: MatchResult): Observable<Response> {
+    const headers = new Headers([
+      { 'Content-Type': 'application/json' },
+      {'Authorization': this.authenticationBasicService.getCurrentUser().authorization}]
+    );
+    const options = new RequestOptions({ headers: headers });
 
-    // DELETE /admin/{id}
-    public deleteMatchResult(matchResult: MatchResult): Observable<Response> {
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        headers.append('Authorization', this.authenticationBasicService.getCurrentUser().authorization);
-        const options = new RequestOptions({ headers: headers });
-
-        return this.http.delete(`${environment.API}${matchResult.id}`, options)
-            .map((res: Response) => res)
-            .catch((error: any) => Observable.throw(error.json()));
-    }
+    return this.http.delete(`${environment.API}${matchResult.id}`, options)
+      .map((res: Response) => res)
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
 }
 
