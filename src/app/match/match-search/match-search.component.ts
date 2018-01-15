20d3bc81fc80e8caeb2b49cb551a531e5fb8a031
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-16 00:49:23 +0100

    implemented the filtering component

---
+++ src/app/match/match-search/match-search.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {MatchService} from '../Match.service';
+import {Match} from "../Match";
+
+@Component({
+  selector: 'app-public-match-search',
+  templateUrl: './public-match-search.component.html'
+})
+
+export class MatchSearchComponent {
+  @Input()
+  matches: Match[];
+  @Output()
+  onSearchited: EventEmitter<any> = new EventEmitter();
+
+  public errorMessage: string;
+  constructor(private matchService: MatchService,
+              private route: ActivatedRoute) {
+  }
+
+  performSearch(from: Date, to: Date): void {
+    this.matchService.getMatchByStartDate(from.toISOString(), to.toISOString()).subscribe(
+      matches => { this.onSearchited.emit(matches); },
+      error => this.errorMessage = <any>error.message);
+  }
+
+}
