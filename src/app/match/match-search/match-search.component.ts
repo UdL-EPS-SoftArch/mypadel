33e7c154d2ae19319a7667f69897a1da731aa2e3
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-16 13:01:15 +0100

    added the filter option by types

--- src/app/match/match-search/match-search.component.ts
+++ src/app/match/match-search/match-search.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, Input, EventEmitter, Output} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {MatchService} from '../Match.service';
-import {Match} from "../Match";
+import {Match} from '../Match';
+import {PublicMatchService} from '../../public-match/PublicMatch.service';
+import {PrivateMatchService} from '../../private-match/private-match.service';
+import {CustomMatchService} from '../../custom-match/custom-match.service';
 
 @Component({
   selector: 'app-match-search',
@@ -15,14 +18,43 @@ export class MatchSearchComponent {
   onSearchited: EventEmitter<any> = new EventEmitter();
 
   public errorMessage: string;
+
   constructor(private matchService: MatchService,
+              private publicMatchService: PublicMatchService,
+              private customMatchService: CustomMatchService,
+              private privateMatchService: PrivateMatchService,
               private route: ActivatedRoute) {
   }
 
   performSearch(from: Date, to: Date): void {
     this.matchService.getMatchByStartDate(from.toISOString(), to.toISOString()).subscribe(
-      matches => { this.onSearchited.emit(matches); },
+      matches => {
+        this.onSearchited.emit(matches);
+      },
       error => this.errorMessage = <any>error.message);
   }
 
+  searchPublic(): void {
+    this.publicMatchService.getAllPublicMatches().subscribe(
+      matches => {
+        this.onSearchited.emit(matches);
+      },
+      error => this.errorMessage = <any>error.message);
+  }
+
+  searchPrivate(): void {
+    this.privateMatchService.getAllPrivateMatches().subscribe(
+      matches => {
+        this.onSearchited.emit(matches);
+      },
+      error => this.errorMessage = <any>error.message);
+  }
+
+  searchCustom(): void {
+    this.customMatchService.getAllCustomMatches().subscribe(
+      matches => {
+        this.onSearchited.emit(matches);
+      },
+      error => this.errorMessage = <any>error.message);
+  }
 }
