b3ddc2ebf1f66d83a38e0681d748e9b253804191
cclauwers <cclauwers@gmail.com>
2018-01-16 19:28:22 +0100

    reservation detail created

---
+++ e2e/pages/reservation/reservation-detail.page.ts
@@ -0,0 +1,32 @@
+import {element, by, protractor,ElementFinder} from 'protractor';
+import {promise} from 'selenium-webdriver';
+
+
+export class ReservationDetailPage {
+
+  private detailsBody: ElementFinder;
+  private detailsHeading: ElementFinder;
+
+  constructor() {
+    this.detailsHeading = element.all(by.css('div.panel-heading')).first();
+    this.detailsBody = element.all(by.css('div.panel-body')).first();
+  }
+
+  getStartDate(): promise.Promise<string> {
+    return this.detailsBody
+      .element(by.cssContainingText('h5', 'StartDate:'))
+      .element(by.xpath('following-sibling::p')).getText();
+  }
+
+  getDuration(): promise.Promise<string> {
+    return this.detailsBody
+      .element(by.cssContainingText('h5', 'Duration'))
+      .element(by.xpath('following-sibling::p')).getText();
+  }
+
+  getCourtType(): promise.Promise<string> {
+    return this.detailsBody
+      .element(by.cssContainingText('h5', 'CourtType:'))
+      .element(by.xpath('following-sibling::p')).getText();
+  }
+}
