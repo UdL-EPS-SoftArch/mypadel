2731468270fb4858b8d4f015322c3ac8beaa37b5
Roger Arqués <rarquesvall@gmail.com>
2018-01-16 19:31:35 +0100

    Logical representation of private match form page

---
+++ e2e/pages/private-match/private-match-form.page.ts
@@ -0,0 +1,58 @@
+import {by, element, protractor} from 'protractor';
+import {promise} from 'selenium-webdriver';
+
+
+export class PrivateMatchFormPage {
+
+  private form;
+  private startDate;
+  private duration;
+  private courtType;
+  private registerButton;
+
+  constructor() {
+    this.form = element(by.id('match-form'));
+    this.duration = this.form.element(by.css('option[value="PT60M"]')).click();
+    this.courtType = this.form.element(by.css('option[value="INDOOR"]')).click();
+    this.form.element(by.id('startDate')).click();
+    this.startDate = this.form.element(by.className('owl-calendar-outFocus')).click();
+    this.registerButton = this.form.element(by.tagName('button'));
+  }
+
+  getStartDate(): promise.Promise<string> {
+    return this.startDate.getText();
+  }
+
+  getDuration(): promise.Promise<string> {
+    return this.duration.getText();
+  }
+
+  getCourtType(): promise.Promise<string> {
+    return this.courtType.getText();
+  }
+
+  getLevel(): promise.Promise<string> {
+    return this.duration.getText();
+  }
+
+  setStartDate(value: string): promise.Promise<void> {
+    this.startDate.sendKeys(protractor.Key.DELETE);
+    return this.startDate.clear().sendKeys(value);
+  }
+
+  setDuration(value: string): promise.Promise<void> {
+    return this.duration = this.form.element(by.css('option[value="' + value + '"]')).click();
+  }
+
+  setCourtType(value: string): promise.Promise<void> {
+    return this.duration = this.form.element(by.css('option[value="' + value + '"]')).click();
+  }
+
+  setLevel(value: string): promise.Promise<void> {
+    return this.duration = this.form.element(by.css('option[value="' + value + '"]')).click();
+  }
+
+  submitForm(): promise.Promise<void> {
+    return this.registerButton.click();
+  }
+}
